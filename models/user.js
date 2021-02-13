f982471c66142c682b0b5f657836002bd0f62b20
fabschurt13 <a4844672@sina.com>
2021-02-13 09:39:12 +0800

    commit

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  identifant: { type: String, required: true },
-  email: String,
-  password: String,
-  phoneNumber: Number,
-  profilePicture: String,
-  FirstName: String,
-  LastName: String,
-  social: Boolean,
-  role: String,
-  verified: { type: Boolean, default: false },
-  className: String,
+    identifant: { type: String, required: true },
+    email: String,
+    password: String,
+    phoneNumber: Number,
+    profilePicture: { type: String, default: "default.png" },
+    FirstName: String,
+    LastName: String,
+    social: Boolean,
+    role: String,
+    verified: { type: Boolean, default: false },
+    className: String,
 });
 module.exports = mongoose.model("user", userSchema);
