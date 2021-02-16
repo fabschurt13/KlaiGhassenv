0fdf2d907b804a2c91db0a9d22666b2b5517bc2c
fabschurt13 <a4844672@sina.com>
2021-02-17 07:27:12 +0800

    pdf

--- routes/uploadDownload.js
+++ routes/uploadDownload.js
@@ -2,61 +2,53 @@ var express = require("express");
 var router = express.Router();
 const multer = require("multer");
 
-
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        var filetype = "";
-        var fileExtension = "";
-        if (file.mimetype === "image/gif") {
-            filetype = "image-";
-            fileExtension = "gif";
-        }
-        if (file.mimetype === "image/png") {
-            filetype = "image-";
-            fileExtension = "png";
-        }
-        if (file.mimetype === "image/jpeg") {
-            filetype = "image-";
-            fileExtension = "jpeg";
-        }
-        if(file.mimetype=== "pdf"){
-filetype="pdf-"
-fileExtension= "pdf"
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    var filetype = "";
+    var fileExtension = "";
+    if (file.mimetype === "image/gif") {
+      filetype = "image-";
+      fileExtension = "gif";
+    }
+    if (file.mimetype === "image/png") {
+      filetype = "image-";
+      fileExtension = "png";
+    }
+    if (file.mimetype === "image/jpeg") {
+      filetype = "image-";
+      fileExtension = "jpeg";
+    }
+    if (file.mimetype === "pdf") {
+      filetype = "pdf-";
+      fileExtension = "pdf";
+    }
 
-        }
-
-        cb(null, filetype + Date.now() + "." + fileExtension);
-        h = cb;
-    },
+    cb(null, filetype + Date.now() + "." + fileExtension);
+    h = cb;
+  },
 });
 var upload = multer({
-    storage: storage,
+  storage: storage,
 });
 
-
-
-
-
-router.get("/download/:nom", function(req, res) {
-    let nom = req.params.nom;
-    const file = picsPath + "/" + nom;
-    console.log(file, "hy");
-    res.sendFile(file); // Set disposition and send it.
+router.get("/download/:nom", function (req, res) {
+  let nom = req.params.nom;
+  const file = picsPath + "/" + nom;
+  console.log(file, "hy");
+  res.sendFile(file); // Set disposition and send it.
 });
 
-router.post("/file", upload.single("file"), function(req, res, next) {
-    if (!req.file) {
-        res.status(500);
-        return next(err);
-    }
-    res.json({
-        img: req.file.filename,
-    });
+router.post("/file", upload.single("file"), function (req, res, next) {
+  if (!req.file) {
+    res.status(500);
+    return next(err);
+  }
+  res.json({
+    img: req.file.filename,
+  });
 });
-
-
 
 module.exports = router;
